d5ffc4c6e3edee8f90965ad55ed74e5cbca277b6
juliazaba <julia.zaba@outlook.com>
2024-06-29 13:03:16 -0700

    Initial commit

---
+++ 02-TypesAndVariables/textbook.py
@@ -0,0 +1,29 @@
+'''
+imie = input('Podaj swoje imie: ')
+print(f'Witaj {imie}!')
+
+godziny = float(input('Podaj liczbe godzin: '))
+stawka = float(input('Podaj stawke godzinowa: '))
+wynagrodzenie = godziny * stawka 
+print(f'Wynagrodzenie: {wynagrodzenie}')
+
+'''
+
+'''width = 17 
+height = 12.0'''
+
+'''wartosc = width//2
+print(f'wartosc = {wartosc}, typ = {type(wartosc)}')'''
+
+'''wartosc = width/2.0
+print(f'wartosc = {wartosc}, typ = {type(wartosc)}')'''
+
+'''wartosc = height/3
+print(f'wartosc = {wartosc}, typ = {type(wartosc)}')'''
+
+'''wartosc = 1 + 2 * 5
+print(f'wartosc = {wartosc}, typ = {type(wartosc)}')'''
+
+celsjusz = int(input('Podaj temperature: '))
+far = (2 * celsjusz) + 30 
+print(f'Temperatura w F: {far}')
